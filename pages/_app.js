84c55a82b152bbcee4aed2fedfa2312f83fc5f52
Alex Kuzmin <kuzmin@kreitai.com>
2021-09-03 17:00:07 +0800

    Add NFTs loading function
    Added basic navigation.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+/*
+ * Copyright (c) 2021 Alexander Kuzmin
+ * SPDX-License-Identifier: MIT
+ */
+import "../styles/globals.css";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">Secondary Marketplace</p>
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-6 text-pink-500">Home</a>
+          </Link>
+          <Link href="/create-item">
+            <a className="mr-6 text-pink-500">Sell Digital Asset</a>
+          </Link>
+          <Link href="/my-assets">
+            <a className="mr-6 text-pink-500">My Digital Assets</a>
+          </Link>
+          <Link href="/creator-dashboard">
+            <a className="mr-6 text-pink-500">Creator D ashboard</a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />;
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
