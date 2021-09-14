54541ed29e33eb4110653f2a79d7c3a3d19f8f6c
Alex Kuzmin <kuzmin@kreitai.com>
2021-09-14 23:49:51 +0800

    Write contract addresses to config upon deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: MIT
  */
 const hre = require("hardhat");
+const fs = require("fs");
 
 async function main() {
   const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
@@ -14,10 +15,16 @@ async function main() {
   const nft = await NFT.deploy(nftMarket.address);
   await nft.deployed();
   console.log("NFT deployed to:", nft.address);
+
+  let config = `
+  export const nftmarketaddress = "${nftMarket.address}"
+  export const nftaddress = "${nft.address}"
+  `;
+
+  let data = JSON.stringify(config);
+  fs.writeFileSync("config.js", JSON.parse(data));
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
